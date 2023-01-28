8480b09269bfed6841ca007d04967819937512d3
Raghav Srivastava <raghavsrivastava1183@gmail.com>
2023-01-28 23:16:26 +0530

    Initial commit

---
+++ frontend/js/index.js
@@ -0,0 +1,3 @@
+const pledge_body = Array.from(document.getElementsByTagName("span"))[0];
+pledge_body.addEventListener("click",()=>
+{pledge_body.innerHTML=`<input type="text"></input>`;pledge_body.removeEventListener("click")})
