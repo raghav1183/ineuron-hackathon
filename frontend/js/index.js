4fdb4517bbbe1deb2e3fe3c4e65d03097a0d6b85
Raghav Srivastava <raghavsrivastava1183@gmail.com>
2023-01-29 14:15:29 +0530

    sahihai

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,5 @@
-const pledge_body = Array.from(document.getElementsByTagName("span"))[0];
-pledge_body.addEventListener("click",()=>
-{pledge_body.innerHTML=`<input type="text"></input>`;pledge_body.removeEventListener("click")})
+// const pledge_body = Array.from(document.getElementsByTagName("span"))[0];
+// pledge_body.addEventListener("click",()=>
+// {pledge_body.innerHTML=`<input type="text"></input>`;pledge_body.removeEventListener("click")})
+
+// alert("hi")
